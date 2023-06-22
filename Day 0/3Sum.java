a038a2265abc7fd6f9e62595f8917ea8aa0d8364
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-06-23 00:44:25 +0530

    Update 3Sum.java

--- Day 0/3Sum.java
+++ Day 0/3Sum.java
@@ -1,1 +1,27 @@
+//sum combination of three numbers
+
+import java.util.Scanner;
+
+public class SumCombinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int c = scanner.nextInt();
+
+        int sum1 = a + b;
+        int sum2 = a + c;
+        int sum3 = b + c;
+      
+        System.out.println("Sum combinations of three numbers:");
+
+        System.out.println("\n"+sum1+"\n"+sum2+"\n"+sum3);
+    }
+}
 
