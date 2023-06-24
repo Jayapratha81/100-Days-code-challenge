32d864e979e6ab79f21aad974125984b07243dfa
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-06-24 16:22:43 +0530

    Create waterreq.java

---
+++ Day 2/waterreq.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class waterreq
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int t,n,i;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+		for(i=0;i<t;i++)
+		{
+		    n=sc.nextInt();
+		    System.out.println(2*n);
+		}
+	}
+}
