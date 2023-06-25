0c3f0ea890e15f04c04232ce932098e45527148c
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-06-25 19:22:55 +0530

    Create emailRem.java

---
+++ Day 3/emailRem.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class emailRem
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,u;
+		Scanner sc=new Scanner(System.in);
+	  n=sc.nextInt();
+		u=sc.nextInt();
+		System.out.println(n-u);
+		
+	}
+}
