a57004fcb4317d83db29a430f9bfaebb61689010
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-06-27 20:12:33 +0530

    Create fill.java

---
+++ Day 4/fill.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class fill
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int k,x,t;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    k=sc.nextInt();
+		    x=sc.nextInt();
+		    System.out.println(k-x);
+		}
+	}
+}
