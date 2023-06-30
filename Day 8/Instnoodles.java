f2df728db2e93ce7d21a4d66b1055b301c03be49
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-06-30 22:38:44 +0530

    Create Instnoodles.java

---
+++ Day 8/Instnoodles.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Instnoodles
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x,y;
+		Scanner sc=new Scanner(System.in);
+		x=sc.nextInt();
+		y=sc.nextInt();
+		System.out.println(x*y);
+	}
+}
