5dc1d76693724ae14e3050d293f955229dcee615
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-07-13 14:53:26 +0530

    Create numberMirror.java

---
+++ Day 17/numberMirror.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class numberMirror
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.println(n);
+	}
+}
