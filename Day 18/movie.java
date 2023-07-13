0da9a282650b7d7e16933e9ad702eb6f16636a37
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-07-13 14:57:45 +0530

    Create movie.java

---
+++ Day 18/movie.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class movie
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x,y,z;
+		Scanner sc=new Scanner(System.in);
+		x=sc.nextInt();
+		y=sc.nextInt();
+		z=y/2;
+		System.out.println(z+(x-y));
+	}
+}
