9f50e3566cc16305ae4e9e067985e065b79695a7
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-07-20 13:59:42 +0530

    Create pattern4.java

---
+++ Day 26/pattern4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class pattern4
+  {
+    void printTriangle(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            {
+            System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+       
+    }
+}
