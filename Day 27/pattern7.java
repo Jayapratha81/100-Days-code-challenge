bbde0cc8f8abb0eeb0b42ae28373305d26230300
Jayapratha81 <111642309+Jayapratha81@users.noreply.github.com>
2023-07-27 23:11:44 +0530

    Create pattern7.java

---
+++ Day 27/pattern7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+
+    void printTriangle(int n) {
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<i;j++)
+            {
+               System.out.print("*");
+            }
+            for(int j=1;j<=i;j++)
+            {
+               System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
